bc0eba28bf342d508ab22f31ce80125e5c06ac53
Alain Lemaire <jaxdid@gmail.com>
2018-06-05 23:38:49 +0100

    Updates BeerGrid to fetch beers from API and then display total count

--- src/components/BeerGrid.js
+++ src/components/BeerGrid.js
@@ -1,7 +1,30 @@
 import React, { Component } from 'react'
 
 export default class BeerGrid extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      beers: []
+    }
+  }
+
+  componentDidMount () {
+    this.fetchBeers()
+      .then(beers => this.setState({beers}))
+  }
+
+  async fetchBeers () {
+    const beers = await window.fetch('https://api.punkapi.com/v2/beers/')
+    return beers.json()
+  }
+
   render () {
-    return <div>Beer Grid</div>
+    return (
+      <div className="beer-grid">
+        <div>Beers on tap:</div>
+        <div>{this.state.beers.length || 'Loading...'}</div>
+      </div>
+    )
   }
 }
