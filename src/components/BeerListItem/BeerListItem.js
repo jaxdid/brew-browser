385d7b326f7ccbf523fdcc2c287a22d86f1efa5d
Alain Lemaire <jaxdid@gmail.com>
2018-06-11 11:03:25 +0100

    Adds ABV for each beer

--- src/components/BeerListItem/BeerListItem.js
+++ src/components/BeerListItem/BeerListItem.js
@@ -5,6 +5,7 @@ import './beerListItem.less'
 
 export default function BeerListItem ({ data }) {
   const {
+    abv,
     image_url: imageUrl,
     name,
     tagline,
@@ -13,6 +14,10 @@ export default function BeerListItem ({ data }) {
 
   return (
     <div className="beer-list-item">
+      <div className="beer-abv">
+        {`${abv}%`}
+        <div className="beer-abv-label">ABV</div>
+      </div>
       <div className="beer-image" style={{ backgroundImage: `url(${imageUrl})` }} />
       <h4>{name}</h4>
       <p>{tagline}</p>
